42f0900a1474f3f906c6c289502eb97a0701c15e
gimyojun <gywnsdl5623@gmail.com>
2023-11-06 10:23:58 +0900

    1단계 완료

---
+++ src/main/java/com/ll/App.java
@@ -0,0 +1,24 @@
+package com.ll;
+
+import java.util.Scanner;
+
+public class App {
+    void run(){
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("== 명언 앱 ==");
+            System.out.println("명령)");
+            String order = sc.next();
+
+            if(order.equals("종료")){
+                break;
+            }
+
+
+        }
+
+
+
+
+    }
+}
