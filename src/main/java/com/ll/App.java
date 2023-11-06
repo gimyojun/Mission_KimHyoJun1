708455d4109c25441d17e2ed36dba613e9481101
hyojun kim <37866182+gimyojun@users.noreply.github.com>
2023-11-06 11:40:09 +0900

    1 week (#3)
    * 1단계 완료
    
    * 5단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,8 +1,12 @@
 package com.ll;
 
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
+    ArrayList<Quote> quotesList = new ArrayList<>();
+
     void run(){
         Scanner sc = new Scanner(System.in);
         while(true){
@@ -12,6 +16,21 @@ public class App {
 
             if(order.equals("종료")){
                 break;
+            }else if(order.equals("등록")){
+                String quote;
+                String writer;
+                System.out.println("명언 : ");
+                quote = sc.next();
+                System.out.println("작가 : ");
+                writer = sc.next();
+                Quote quoteTmp = new Quote(quote,writer);
+                quotesList.add(quoteTmp);
+            }else if(order.equals("목록")){
+                System.out.println("번호 / 작가 /명언");
+                for(int i=0;i<quotesList.size();i++){
+                    System.out.println(i + 1+ " / " + quotesList.get(i).writer + " / " + quotesList.get(i).quote);
+                }
+
             }
 
 
---
+++ src/main/java/com/ll/Quote.java
@@ -0,0 +1,11 @@
+package com.ll;
+
+public class Quote {
+    String quote;
+    String writer;
+
+    Quote(String quote, String writer){
+        this.quote=quote;
+        this.writer=writer;
+    }
+}
